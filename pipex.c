ef5b6aef088ed83260df3c919ff6ada31f265780
Houda Harit <hharit@e1r11p4.1337.ma>
2022-01-31 14:03:26 +0100

    leaks

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 13:41:18 by hharit            #+#    #+#             */
-/*   Updated: 2022/01/07 02:06:45 by hharit           ###   ########.fr       */
+/*   Updated: 2022/01/31 13:57:39 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,30 @@ void	ft_initialize(int **p, char  **path, int *fd1, int *fd2, char **argv, char 
 
 void	parent_p(int p0, int p1, int fd1, char *path, char *argv2, char **envp)
 {
+	char	**cmd;
+
 	close(p0);
 	dup2(fd1, 0);
 	dup2(p1, 1);
 	wait(NULL);
-	cmd_execve(get_path(path, argv2), argv2, envp);
+	cmd = ft_split(argv2, 32);
+	path = check_cmd(path, cmd[0]);
+//	free(cmd);
+	cmd_execve(path, argv2, envp);
 }
 
 void	child_p(int p0, int p1, int fd1, int fd2, char *path, char *argv3, char **envp)
 {
+	char	**cmd;
+
 	close(p1);
 	close(fd1);
 	dup2(p0, 0);
 	dup2(fd2, 1);
-	cmd_execve(get_path(path, argv3), argv3, envp);
+	cmd = ft_split(argv3, 32);
+	path = check_cmd(path, cmd[0]);
+	//free(cmd);
+	cmd_execve(path, argv3, envp);
 }
 
 int main(int argc, char **argv, char **envp)
@@ -54,4 +64,6 @@ int main(int argc, char **argv, char **envp)
 		parent_p(p[0], p[1], fd1, path, argv[2], envp);
 	if (fork() == 0)
 		child_p(p[0], p[1], fd1, fd2, path, argv[3], envp);
+
+	system("leaks pipex");
 }
